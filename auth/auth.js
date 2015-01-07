f98888552dfc4a693d306548459c3a67de10d37f
jayaraj-poroor <jayaraj.poroor@gmail.com>
2015-01-07 19:01:09 +0530

    sample auth mod

--- auth/auth.js
+++ auth/auth.js
@@ -1,13 +1,31 @@
+var sysOk = true;
+
 /**
- @auth {a:"local"}
+ @auth "local"
+ @path "/login"
 */
-exports.auth = function(msg, done){
+
+exports.auth = function(msg, done, err){
+	if(!sysOk){
+		err.sys("System error!");
+	}
 	
+	if(msg.username == "admin" && msg.password == "admin"){
+		done({username: "admin"});
+	}else{
+		err.app("Invalid authentication attempt: " + msg.username);
+	}
 }
 
 /**
-  @auth ["google", "facebook", "twitter"]
+  @auth 	  ["google", "facebook", "twitter"]
+  @pathPrefix "/login"
+  @ignore
 */
-exports.providerAuth  = function(msg, done){
-
+exports.providerAuth  = function(msg, done, err){
+	if(msg.profile.email == "xxx@gmail.com"){
+		done({username: msg.profile.email});
+	}else{
+		err("Invalid authentication attempt: " + msg.profile.email);
+	}
 }
