985ebddde92371e622a00c719636186d5c806249
jayaraj-poroor <jayaraj.poroor@gmail.com>
2015-01-16 11:33:22 +0530

    minor changes

--- src/init.js
+++ src/init.js
@@ -1,6 +1,6 @@
 module.exports = function(ctx, done){
 	//access the application config via ctx.config.
 	//store any objects required for later access by route handlers in the ctx object.
-	console.log("Application init called");
+	console.log("Application init called. Is master: " + ctx.config.node.is.master);
 	done();
 }
